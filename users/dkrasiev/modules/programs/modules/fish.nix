94e58c00563c6adf46826806230d2d281e293738
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-05-07 23:22:56 +0400

    feat(users/dkrasiev): add more fish aliases

--- users/dkrasiev/modules/programs/modules/fish.nix
+++ users/dkrasiev/modules/programs/modules/fish.nix
@@ -10,9 +10,12 @@
 
   programs.fish.shellAliases = {
     "emias-shell" = "nix develop ~/nix#emias --command fish";
+
     "nixos-update" = "nix flake update ~/nix";
+    "nixos-upgrade" = "nixos-update && nixos-switch && home-switch";
     "nixos-switch" = "nh os switch";
     "nixos-boot" = "nh os boot";
+    "home-switch" = "nh home switch";
   };
 
   programs.fish.interactiveShellInit = ''
