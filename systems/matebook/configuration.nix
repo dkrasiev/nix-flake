9ebabb899aa5de044cc523e6b4ee3c4b6218b02d
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-06-15 15:10:10 +0400

    feat(matebook): return default configuration

--- systems/matebook/configuration.nix
+++ systems/matebook/configuration.nix
@@ -2,9 +2,10 @@
   imports = [
     ./hardware-configuration.nix
 
-    ../default/base-configuration.nix
+    # ../default/base-configuration.nix
+    # ../default/managers/sddm.nix
+    # ../default/managers/hyprland.nix
 
-    ../default/managers/sddm.nix
-    ../default/managers/hyprland.nix
+    ../default/configuration.nix
   ];
 }
