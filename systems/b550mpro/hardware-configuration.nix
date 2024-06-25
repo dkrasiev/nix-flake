dc671d2e26c7f7260cf1425d31562c517e51fabb
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-06-25 10:01:56 +0400

    feat(systems/b550mpro): update hardware-configuration.nixremove grub

--- systems/b550mpro/hardware-configuration.nix
+++ systems/b550mpro/hardware-configuration.nix
@@ -3,31 +3,36 @@
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
 
-let
-  rootPartition = "/dev/disk/by-uuid/bddfb118-1e1a-4307-8f61-c9dd6f41b62c";
-  bootPartition = "/dev/disk/by-uuid/1EEB-502D";
-in
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  # pc with 7800 xt not booting with default value
-  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = rootPartition;
+    { device = "/dev/disk/by-uuid/ac46f06e-0c2f-4ce8-adb2-fb44bb45b11d";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = bootPartition;
+    { device = "/dev/disk/by-uuid/CD17-C484";
       fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
