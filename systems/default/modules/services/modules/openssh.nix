46cc3f7c59491e6724bbfea5d25f36a015f256e9
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-07-30 23:44:37 +0400

    feat(openssh): enable X11Forwarding

--- systems/default/modules/services/modules/openssh.nix
+++ systems/default/modules/services/modules/openssh.nix
@@ -6,7 +6,7 @@
       PasswordAuthentication = true;
       AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
       UseDns = true;
-      X11Forwarding = false;
+      X11Forwarding = true;
       PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
     };
   };
