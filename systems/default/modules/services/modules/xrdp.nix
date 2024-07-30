f93d8f9a716bc768ae54908e2f65d218c6a5dcda
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-07-30 23:23:10 +0400

    feat(xrdp): change default window manager

--- systems/default/modules/services/modules/xrdp.nix
+++ systems/default/modules/services/modules/xrdp.nix
@@ -1,5 +1,5 @@
 {
   services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "startplasma-wayland";
+  services.xrdp.defaultWindowManager = "startplasma-x11";
   services.xrdp.openFirewall = true;
 }
