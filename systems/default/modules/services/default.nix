3e980531dd711aec21195e0229efa1192371ac18
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-07-30 12:56:42 +0400

    feat: add openssh and xrdp modules to services/

--- systems/default/modules/services/default.nix
+++ systems/default/modules/services/default.nix
@@ -1,8 +1,10 @@
 {
   imports = [
     ./modules/libinput.nix
+    ./modules/openssh.nix
     ./modules/printing.nix
     ./modules/samba.nix
+    ./modules/xrdp.nix
     ./modules/xserver.nix
   ];
 }
