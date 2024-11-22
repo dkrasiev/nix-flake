3f8b1ba2021db796e118cd9ac08d594100a980ea
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-11-22 13:13:14 +0400

    feat: switch to stable 24.05

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "dkrasiev's flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, nixpkgs }:
